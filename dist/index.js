5c5e0e3b8f9eb02b313ba9dbc4d9b37755c808d0
devUserContact <developer.user.contact@protonmail.com>
2023-04-27 09:49:13 -0700

    connect to curvefi api

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
-import { ChainId } from '@uniswap/sdk';
-console.log(`The chainId of mainnet is ${ChainId.MAINNET}.`);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import curve from '@curvefi/api';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const apiKey = process.env.QUICKNODE_KEY;
+const apiUrl = process.env.QUICKNODE_URL;
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield curve.init('JsonRpc', { url: apiUrl, privateKey: '' });
+    yield curve.factory.fetchPools();
+    yield curve.cryptoFactory.fetchPools();
+    curve.getPoolList();
+}))();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-import { ChainId } from '@uniswap/sdk'
-console.log(`The chainId of mainnet is ${ChainId.MAINNET}.`)
+import curve from '@curvefi/api'
+import Web3 from 'web3'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const apiKey = process.env.QUICKNODE_KEY;
+const apiUrl = process.env.QUICKNODE_URL;
+
+(async () => {
+  await curve.init('JsonRpc', { url: apiUrl, privateKey: ''})
+  await curve.factory.fetchPools()
+  await curve.cryptoFactory.fetchPools()
+  curve.getPoolList()
+})()
